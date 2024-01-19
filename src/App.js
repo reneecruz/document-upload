2dd602ae52b8d85ea7bbd75fec17607256691a19
Renée Cruz <reneecruz@Renees-MBP.attlocal.net>
2024-01-19 12:46:50 -0500

    add Form component, FileUploader component, styles.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import React from "react";
 
-function App() {
+import Form from "./components/Form.js";
+import FileUploader from "./components/FileUploader.js";
+
+export default function App() {
+  const onUpload = (files) => {
+    console.log(files);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Document Upload</h2>
+      <Form onUpload={onUpload} />
+      <FileUploader onUpload={onUpload} />
     </div>
   );
 }
 
-export default App;
